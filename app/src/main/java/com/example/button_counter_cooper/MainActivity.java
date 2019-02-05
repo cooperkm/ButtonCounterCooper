afaa760914c7d8ab2d200531c47d72728db01a8f
cooperkm <cooperkm@g.cofc.edu>
2019-02-04 19:53:47 -0500

    Completed all requirements for simple button counter app

--- app/src/main/java/com/example/button_counter_cooper/MainActivity.java
+++ app/src/main/java/com/example/button_counter_cooper/MainActivity.java
@@ -2,12 +2,84 @@ package com.example.button_counter_cooper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    int count = 0;
+
+    Button incButton;
+    Button decButton;
+    Button resButton;
+    TextView counter;
+
+
+    // to save state, onSavedInstance state, onRestoreInstanceState
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = (TextView) findViewById(R.id.counter);
+
+        incButton = (Button) findViewById(R.id.IncButton);
+        incButton.setOnClickListener(this);
+
+        decButton = (Button) findViewById(R.id.DecButton);
+        decButton.setOnClickListener(this);
+
+        resButton = (Button) findViewById(R.id.ResButton);
+        resButton.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == incButton){
+            count++;
+            counter.setText("" + count);
+        }
+
+        if(v == decButton){
+            count--;
+            counter.setText("" + count);
+        }
+
+        if(v == resButton){
+            count = 0;
+            counter.setText("" + count);
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        int counterInt = count;
+        outState.putInt("counterIntKey", counterInt);
+
+        String counterStr = counter.getText().toString();  //need to save state of counter
+        outState.putString("counterStrKey", counterStr);
+
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle bundle){
+        super.onRestoreInstanceState(bundle);
+
+        int counterInt = bundle.getInt("counterIntKey");
+        count = counterInt;
+
+        String counterStr = bundle.getString("counterStrKey");
+        counter.setText(counterStr);
+
     }
 }
